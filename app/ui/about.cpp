8def193bbf8a743a50bc0666675a8518374932cc
bytemarx <100504683+bytemarx@users.noreply.github.com>
2024-01-04 02:53:57 -0500

    Added attributions to About dialog

--- app/ui/about.cpp
+++ app/ui/about.cpp
@@ -1,3 +1,6 @@
+#include <QFile>
+#include <QMessageBox>
+
 #include "about.hpp"
 
 #include "config.h"
@@ -5,9 +8,23 @@
 #define APP_VERSION "v" ORBITAL_PROJECT_VERSION
 
 
+static QString
+attributions()
+{
+    QFile file{":/res/attributions.txt"};
+    if (!file.open(QIODevice::ReadOnly))
+        QMessageBox::information(0, "error", file.errorString());
+    QTextStream in{&file};
+    QString content = in.readAll();
+    file.close();
+    return content;
+}
+
+
 About::About(QWidget* parent)
-    : QDialog(parent)
+    : QDialog{parent}
 {
     this->ui.setupUi(this);
     this->ui.label_version->setText(QString{APP_VERSION});
+    this->ui.plainTextEdit_attributions->setPlainText(attributions());
 }
