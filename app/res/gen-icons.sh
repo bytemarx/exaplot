efc0c2a4f9e555d953c29ded7a2e286ca6271765
bytemarx <100504683+bytemarx@users.noreply.github.com>
2024-04-02 23:24:14 -0400

    app: Updated icon

---
+++ app/res/gen-icons.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+inkscape --export-filename=icon-128.png --export-type=png --export-width=128 --export-height=128 icon.svg
+inkscape --export-filename=icon-256.png --export-type=png --export-width=256 --export-height=256 icon.svg
