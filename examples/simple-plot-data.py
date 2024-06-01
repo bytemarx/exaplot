afc0bc3fb5ea97bd0e216378e7de5bf51a0d227f
bytemarx <100504683+bytemarx@users.noreply.github.com>
2024-06-01 01:15:48 -0400

    python: Added example that saves data

---
+++ examples/simple-plot-data.py
@@ -0,0 +1,33 @@
+import math
+import pathlib
+import time
+
+from exaplot import RunParam, init, plot, stop, datafile
+
+datafile(
+    enable=True,
+    path=lambda: pathlib.Path(f"data-{int(time.time())}.hdf5"),
+    prompt=True
+)
+init(duration=RunParam(float, display="Duration (s)"))
+plot = plot[1]
+plot.title = "Simple Plot Example"
+plot.two_dimen.x_range = -2.5, 2.5
+plot.two_dimen.y_range = -1, 1
+plot.two_dimen.autorescale_axes = True
+
+
+def run(duration: float):
+    x_i = -2.5
+    x_f = 2.5
+    x_delta = 0.001
+    t_delta = duration / ((x_f - x_i) / x_delta)
+
+    def f(x):
+        return math.sin(20 * math.pi * x) * math.exp(-(x**2))
+
+    x = x_i
+    while x <= x_f and not stop():
+        plot(x, f(x))
+        time.sleep(t_delta)
+        x += x_delta
