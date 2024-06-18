3bf16b6748721444cc3a368208fde9900babd466
bytemarx <100504683+bytemarx@users.noreply.github.com>
2024-06-17 21:58:56 -0400

    app: Updated About dialog
    Added acknowledgments and replaced plain text widget with text browser
    for enhanced formatting.

--- app/ui/about.cpp
+++ app/ui/about.cpp
@@ -16,23 +16,9 @@
 #define APP_VERSION "v" EXAPLOT_PROJECT_VERSION
 
 
-static QString
-attributions()
-{
-    QFile file{":/res/attributions.txt"};
-    if (!file.open(QIODevice::ReadOnly))
-        QMessageBox::information(0, "error", file.errorString());
-    QTextStream in{&file};
-    QString content = in.readAll();
-    file.close();
-    return content;
-}
-
-
 About::About(QWidget* parent)
     : QDialog{parent}
 {
     this->ui.setupUi(this);
     this->ui.label_version->setText(QString{APP_VERSION});
-    this->ui.plainTextEdit_attributions->setPlainText(attributions());
 }
